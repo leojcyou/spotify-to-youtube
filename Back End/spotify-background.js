95c78adcfbed7482859e69e373b2305460cb89a7
Leoy456 <leoyou2004@gmail.com>
2022-12-23 18:30:20 -0500

    This is bad

--- Back End/spotify-background.js
+++ Back End/spotify-background.js
@@ -1,8 +1,54 @@
 //const clientId = 
 let isSignedIn = false;
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.message == "login") {
+function authenticateSpotify() {
 
-    }
-})
+    var client_id = 'd502e1de8496425e9a6b3c792ae9df0d';
+    var redirect_uri = 'https://kmiobbpfbiijebejmcmjgjjidpfnlkii.chromiumapp.org/';
+    var state = generateRandomString(16);
+    localStorage.setItem(stateKey, state);
+    var scope = 'playlist-read-private playlist-read-collaborative';
+
+    //generates url with access_token
+    var url = 'https://accounts.spotify.com/authorize';
+    url += '?response_type=token';
+    url += '&client_id=' + encodeURIComponent(client_id);
+    url += '&scope=' + encodeURIComponent(scope);
+    url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+    url += '&state=' + encodeURIComponent(state);
+
+    console.log("hi");
+    // let accessLink = chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    //     if (request.message == "login") {
+    //         chrome.identity.launchWebAuthFlow(
+    //         {
+    //             url: createSpotifyUrl(),
+    //             interactive: true
+    //         },
+    //         redirectOnFailure
+    //         )
+    //     }
+    
+    //     return true;
+    // })
+
+    // return accessLink;
+}
+
+function readPlaylist(playlistId) {
+
+}
+
+
+
+
+export {authenticateSpotify, token};
+
+
+
+
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//     if (request.message == "login") {
+
+//     }
+// })
