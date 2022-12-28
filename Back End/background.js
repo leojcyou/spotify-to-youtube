e65c6b6a12fff768cd4d9ce5e6b84e5f1d414378
qianxug <qianxug@gmail.com>
2022-12-27 23:56:26 -0500

    fixed code formatting + implemented link input check. TO-DO: implement verification at each step, encryption, pass over inputted playlist link, html/css to style success/failure messages

--- Back End/background.js
+++ Back End/background.js
@@ -1,204 +1,188 @@
-//Mainloop
-//Google
-const API_KEY = "AIzaSyDjeaR3-LeYmrAIw-VwQ_V2YuEPd6KHQC0"
+const YOUTUBE_API_KEY = "AIzaSyDjeaR3-LeYmrAIw-VwQ_V2YuEPd6KHQC0"
 const SPOTIFY_PLAYLIST_ID = "7bjBfZIF7ylSBVV0aqAUBx" 
-let playlistName = ""
 
 const songNames = []
 const artistNames = []
-const ytIds = []
+const youtubeSongIds = []
+
+let youtubePlaylistName = ""
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.message == "spotify login") {
+    if (request.message == "spotify") {
         chrome.identity.launchWebAuthFlow(
-        {
-            url: createSpotifyUrl(),
-            interactive: true
-        }, spotifyCallback
-        )
-    }
-    if (request.message == "google token"){
-        console.log("hello")
-        chrome.identity.getAuthToken(
             {
+                url: createSpotifyUrl(),
                 interactive: true
-            }, googleCallback
+            }, 
+            spotifyCallback
         )
-        // sendResponse(true) //unsure what this does atm
     }
 })
 
-
-
-
-//Spotify
-function createSpotifyUrl() {
-    const spotifyClientId = "d502e1de8496425e9a6b3c792ae9df0d";
-    const state = "meet" + Math.random().toString(36).substring(2, 15);
-    const spotifyScope = "playlist-read-private playlist-read-collaborative";
-    const showDialog = "true";
-    const responseType = "token";
-    const redirectUri = "https://ddhlalnafmgfpodpaiiheghpeochgflc.chromiumapp.org/"; //the id may be different?
-
-    let spotifyUrl = "https://accounts.spotify.com/authorize?";
-    spotifyUrl += "client_id=" + encodeURIComponent(spotifyClientId);
-    spotifyUrl += "&response_type=" + encodeURIComponent(responseType);
-    spotifyUrl += "&redirect_uri=" + encodeURIComponent(redirectUri);
-    spotifyUrl += "&state=" + encodeURIComponent(state);
-    spotifyUrl += "&scope=" + encodeURIComponent(spotifyScope);
-    spotifyUrl += "&show_dialog=" + encodeURIComponent(showDialog);
-    
-    console.log(spotifyUrl);
-    return spotifyUrl;
-}
-
-function parseUrl(accessUrl){
-    let accessToken = accessUrl.split('=')[1];
-    accessToken = accessToken.split('&')[0];
-    return accessToken;
-}
-
-async function getName(token, playlistId){//
-    let playlistUrl = `https://api.spotify.com/v1/playlists/${playlistId}`;
-
-    const response = await fetch(playlistUrl, {//
-        method: 'GET',
-        headers: { 
-            "Accept" : "application/json",
-            "Content-Type":"application/json",
-            "Authorization" : "Bearer " + token
-        }
-    });
-    const data = await response.json();
-    console.log(data)
-
-    playlistName = data.name + " | Import from Spotify"
-
-    return;
-}
-
-async function importPlaylist(token, playlistId){//
-    let limit = 2;
-    let playlistUrl = `https://api.spotify.com/v1/playlists/${playlistId}/tracks`;
-
-    const response = await fetch(playlistUrl, {//
-        method: 'GET',
-        headers: { 
-            "Accept" : "application/json",
-            "Content-Type":"application/json",
-            "Authorization" : "Bearer " + token
-        }
-    });
-    const data = await response.json();
-    console.log(data)
-
-    if(data.total < limit) limit = data.total;
-    
-    for (let i = 0; i < limit ; i++)
-    {
-        songNames[i] = data.items[i].track.name;
-        artistNames[i] = data.items[i].track.artists[0].name;
-
-        console.log(data.items[i].track.name);
-        console.log(data.items[i].track.artists[0].name);
-    }
-
-    return;
-}
-
 async function spotifyCallback(redirectUrl) {
-    if (chrome.runtime.lastError || redirectUrl.includes('callback?error=access_denied')){
-        console.log("no auth");
+    if (chrome.runtime.lastError || redirectUrl.includes("callback?error=access_denied")) {
+        console.log("no auth")
     }
 
-    else
-    {
-        console.log(redirectUrl);
-        console.log("authed");
-        let spotifyAuthToken = parseUrl(redirectUrl);
-        console.log(spotifyAuthToken);
-        await getName(spotifyAuthToken,SPOTIFY_PLAYLIST_ID)
-        await importPlaylist(spotifyAuthToken,SPOTIFY_PLAYLIST_ID);
-        //console.log("hjel;osdfjios")
+    else {
+        let spotifyAuthToken = parseUrl(redirectUrl)
+        await importPlaylist(spotifyAuthToken, "7bjBfZIF7ylSBVV0aqAUBx")
 
         chrome.identity.getAuthToken(
             {
                 interactive: true
-            }, googleCallback
+            }, 
+            youtubeCallback
         )
     }        
-    
-    return
 }
 
-//Youtube
-async function googleCallback(token) {
+async function youtubeCallback(googleAuthToken) {
     if (chrome.runtime.lastError) {
-        console.log("no auth");
+        console.log("no auth")
     }
 
     else {
-        let googleAuthToken = token;
-        console.log(googleAuthToken);
-
-        await searchYoutube(googleAuthToken);
-        console.log("Searching has finished")
-
+        await searchYoutube(googleAuthToken)
         createPlaylist(googleAuthToken)
     } 
 }
 
-async function createPlaylist(token) {
-    // let d = new Date()
-    // let date = d.getFullYear() + "/" + (d.getMonth()+1) + "/" + d.getDate()
-    // let playlistName = "Import from Spotify" + " " + date
+// Spotify
 
-    let endpointUrl = `https://youtube.googleapis.com/youtube/v3/playlists?part=snippet&key=${API_KEY}`
+function createSpotifyUrl() {
+    const SPOTIFY_CLIENT_ID = "d502e1de8496425e9a6b3c792ae9df0d"
+    const STATE = Math.random().toString().substring(2, 18)
+    const SPOTIFY_SCOPE = "playlist-read-private playlist-read-collaborative"
+    const SHOW_DIALOG = "true"
+    const RESPONSE_TYPE = "token"
+    const REDIRECT_URI = "https://ddhlalnafmgfpodpaiiheghpeochgflc.chromiumapp.org/" //the id may be different?
+
+    let spotifyUrl = `https://accounts.spotify.com/authorize?`
+    spotifyUrl += `client_id=` + encodeURIComponent(SPOTIFY_CLIENT_ID)
+    spotifyUrl += `&response_type=` + encodeURIComponent(RESPONSE_TYPE)
+    spotifyUrl += `&redirect_uri=` + encodeURIComponent(REDIRECT_URI)
+    spotifyUrl += `&state=` + encodeURIComponent(STATE)
+    spotifyUrl += `&scope=` + encodeURIComponent(SPOTIFY_SCOPE)
+    spotifyUrl += `&show_dialog=` + encodeURIComponent(SHOW_DIALOG)
+    
+    return spotifyUrl
+}
+
+function parseUrl(accessUrl) {
+    let accessToken = accessUrl.split("=")[1]
+    accessToken = accessToken.split("&")[0]
+
+    return accessToken
+}
+
+async function importPlaylist(spotifyAuthToken, spotifyPlaylistId) {
+    let limit = 2
+    let spotifyPlaylistUrl = `https://api.spotify.com/v1/playlists/${spotifyPlaylistId}/tracks`
+
+    await getName(spotifyAuthToken, spotifyPlaylistId)
+
+    const response = await fetch(spotifyPlaylistUrl, {
+        method: "GET",
+        headers: { 
+            "Accept" : "application/json",
+            "Content-Type":"application/json",
+            "Authorization" : "Bearer" + " " + spotifyAuthToken
+        }
+    })
+
+    const data = await response.json()
+
+    if (data.total < limit) {
+        limit = data.total
+    }
+
+    for (let i = 0; i < limit; i++) {
+        songNames[i] = data.items[i].track.name
+        artistNames[i] = data.items[i].track.artists[0].name
+    }
+}
+
+async function getName(spotifyAuthToken, spotifyPlaylistId) {
+    let spotifyPlaylistUrl = `https://api.spotify.com/v1/playlists/${spotifyPlaylistId}`
+
+    const response = await fetch(spotifyPlaylistUrl, {
+        method: "GET",
+        headers: { 
+            "Accept" : "application/json",
+            "Content-Type":"application/json",
+            "Authorization" : "Bearer" + " " + spotifyAuthToken
+        }
+    })
+
+    const data = await response.json()
+
+    youtubePlaylistName = data.name + " | Import from Spotify"
+}
+
+async function searchYoutube(googleAuthToken) {
+    for (let i = 0; i < songNames.length; i++) {
+        let keyword = getKeyword(artistNames[i], songNames[i])
+        let searchUrl = `https://www.googleapis.com/youtube/v3/search?part=id&maxResults=1&q=${keyword}&key=${YOUTUBE_API_KEY}`
+
+        const response = await fetch(searchUrl, {
+            method: "GET",
+            headers: { 
+                "Accept" : "application/json",
+                "Authorization" : "Bearer" + " " + googleAuthToken
+            }
+        })
+
+        const data = await response.json()
+
+        youtubeSongIds[i] = data.items[0].id.videoId
+    }
+}
+
+async function createPlaylist(googleAuthToken) {
+    let endpointUrl = `https://youtube.googleapis.com/youtube/v3/playlists?part=snippet&key=${YOUTUBE_API_KEY}`
 
     const response = await fetch(endpointUrl, {
-        method: 'POST',
+        method: "POST",
         headers: { 
             "Accept": "application/json",
             "Content-Type": "application/json",
-            "Authorization": "Bearer " + String(token)
+            "Authorization": "Bearer" + " " + googleAuthToken
         },
         body: JSON.stringify(
             {
                 "snippet": {
-                    "title": playlistName
+                    "title": youtubePlaylistName
                 }
             }
         )
-    });
+    })
 
-    const data = await response.json();
+    const data = await response.json()
+    let youtubePlaylistId = data.id
 
-    console.log(data)
-    let playlistId = data.id;
-    console.log("playlist created " + playlistId)
-
-    addSongs(playlistId, token)
+    addSongs(youtubePlaylistId, googleAuthToken)
 }
 
-async function addSongs(playlistId, token) {
-    let endpointUrl = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&part=id&key=${API_KEY}`
+async function addSongs(youtubePlaylistId, googleAuthToken) {
+    let endpointUrl = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&part=id&key=${YOUTUBE_API_KEY}`
 
     for (let i = 0; i < songNames.length; i++) {
         const response = await fetch(endpointUrl, {
-            method: 'POST',
+            method: "POST",
             headers: {
                 "Accept" : "application/json",
                 "Content-Type": "application/json",
-                "Authorization" : "Bearer" + " " + token
+                "Authorization" : "Bearer" + " " + googleAuthToken
             },
             body: JSON.stringify(
                 {
                     "snippet": {
-                        "playlistId": playlistId,
+                        "playlistId": youtubePlaylistId,
                         "position": i,
                         "resourceId": {
                             "kind": "youtube#video",
-                            "videoId": ytIds[i]
+                            "videoId": youtubeSongIds[i]
                         }
                     }
                 }
@@ -207,37 +191,10 @@ async function addSongs(playlistId, token) {
 
         const data = await response.json()
 
-        console.log(data)
-        console.log("song " + i + " added")
-
         //get the data and check the error code, if failed, print to final screen to tell user x song was not added
     }
 }
 
 function getKeyword(artist, song) {
     return artist.replace(" ", "%20") + "%20" + song.replace(" ", "%20")
-}
-
-async function searchYoutube(token) {
-    // console.log("we're in search!")
-
-    for (let i = 0; i < songNames.length; i++) {
-        let searchUrl = "https://www.googleapis.com/youtube/v3/search?part=id&maxResults=1"
-        searchUrl += "&q=" + getKeyword(artistNames[i], songNames[i])
-        searchUrl += "&key=" + API_KEY
-
-        const response = await fetch(searchUrl, {
-            method: 'GET',
-            headers: { 
-                "Accept" : "application/json",
-                "Authorization" : "Bearer" + " " + token
-            }
-        });
-
-        const data = await response.json();
-        console.log(data)
-
-        ytIds[i] = data.items[0].id.videoId
-        console.log(ytIds[i])
-    }
 }
--- Front End/popup-scripts.js
+++ Front End/popup-scripts.js
@@ -1,40 +1,42 @@
 document.getElementById("authenticate_btn").addEventListener("click", main);
 
-function log() {
-  console.log("your mom"); //this was qianxu, not leo
-}
-
-function sendToAuthResponse(response) {
-  if (response.message == "success")
-    window.close();
-}
-
 function sendToAuth(message) {
   chrome.runtime.sendMessage({message: message})
-  chrome.runtime.onMessage.addEventListener((request, sender, sendResponse) => {
-    if (request.message == false)
-    {
-      console.log("failed")
-      return false;
-    }
-    else if (request.message == true)
-    {
-      console.log("succeeded")
-      return true;
-    }
+  // chrome.runtime.onMessage.addEventListener((request, sender, sendResponse) => {
+  //   if (request.message == false)
+  //   {
+  //     console.log("failed")
+  //     return false;
+  //   }
+  //   else if (request.message == true)
+  //   {
+  //     console.log("succeeded")
+  //     return true;
+  //   }
 
-  })
+  // })
 }
 
-function returnFailure() {
-  return
+function isValidLink(input) {
+  try {
+    return Boolean(new URL(input))
+  }
+
+  catch(e) {
+    return false
+  }
 }
 
-function main(){
-  sendToAuth("spotify login")
+function main() {
+  let input = document.getElementById("playlist_link").value
+
+  if (!isValidLink(input)) 
+    alert("bozo")
+  
+  else
+    chrome.runtime.sendMessage({message: "spotify"})
+
   // message login and playlist confirmation
-
-  // console.log("hello there")
 
   // sendToAuth("google token")
 
